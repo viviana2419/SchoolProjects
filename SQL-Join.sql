33c050e742253a887552d40b1cd5d62b9fa2a93f
Jingyi Z <49314681+viviana2419@users.noreply.github.com>
2022-01-28 09:40:10 -0500

    Add files via upload

--- SQL-Join.sql
+++ SQL-Join.sql
@@ -3,6 +3,7 @@ SELECT *
   FROM [SQLTutorial].[dbo].[EmployeeDemographics]
  SELECT *
   FROM [SQLTutorial].[dbo].EmployeeSalary
+
 SELECT *
   FROM [SQLTutorial].[dbo].[EmployeeDemographics]
   inner join SQLTutorial.dbo.EmployeeSalary
@@ -18,3 +19,31 @@ SELECT *
   on EmployeeDemographics.EmployeeID=EmployeeSalary.EmployeeID
   where JobTitle='Salesman'
   group by JobTitle
+
+  SELECT *
+  FROM [SQLTutorial].[dbo].[EmployeeDemographics]
+  union--combine everything with no duplicates
+  --union all(everything including duplicates)
+ SELECT *
+  FROM [SQLTutorial].[dbo].EmployeeSalary
+
+  SELECT FirstName, LastName, Age,
+  case
+	when Age=38 then 'stanley'
+	when Age>30 then 'old'
+	else 'baby'
+  end
+  FROM [SQLTutorial].[dbo].[EmployeeDemographics]
+  WHERE Age is not null
+  order by Age
+
+  SELECT FirstName, LastName, JobTitle,Salary,
+  case
+	when JobTitle='Salesman' then Salary+(Salary*.10)
+	when JobTitle='Accountant' then Salary+(Salary*.05)
+	else Salary+(Salary*.03)
+  end as SalaryAfterRaise
+  FROM [SQLTutorial].[dbo].[EmployeeDemographics]
+  join SQLTutorial.dbo.EmployeeSalary
+  on EmployeeDemographics.EmployeeID=EmployeeSalary.EmployeeID
+ 
