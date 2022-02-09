4d193338a085cabaaec1bf69661c01e7edf04b05
Jingyi Z <49314681+viviana2419@users.noreply.github.com>
2022-02-09 09:54:15 -0500

    Add files via upload
    -In the titanic case, use Categorical Data, Seaborn – Graph and Group By function

--- Pandas Exercise.py
+++ Pandas Exercise.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+
 movies_data=pd.read_csv('movies_data.csv')
 print(movies_data.shape)#data shape
 print(movies_data.shape[1]) #check # of columns
@@ -16,6 +18,15 @@ a = movies_rank.sort_values(by='popularity', ascending=False)
 
 p=movies_data.plot.scatter(x='budget', y='popularity')
 
+#titanic 
+t=pd.read_csv('titanic.csv')
+dummy_embarked=pd.get_dummies(t.Embarked, prefix='em')
+t=t.join(dummy_embarked)
+t.to_csv('titanic.csv')
 
+sns.catplot(x="Embarked", y="Survived", kind="bar", data=t)
+sns.catplot(x="Sex", y="Survived", hue="Embarked", kind="bar", data=t)
+#interpretation: female has higher survival rate, C class survival rate is highest among all
 
-
+t['PassengerId'].groupby([t['Pclass']]).count()
+t['Age'].groupby([t['Sex'],t['Pclass']]).mean()
